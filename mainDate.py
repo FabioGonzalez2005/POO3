8eae83a01187f42a3a0434ff363599477c27777e
FabioGonzalez2005 <fabiogonzalezw@gmail.com>
2024-04-02 12:24:11 +0100

    Ponemos ejemplo

--- mainDate.py
+++ mainDate.py
@@ -1,8 +1,8 @@
 from date import Date
 
 def main():
-    Date(3, 4, 2005)
-    print(Date.is_leap_year(2005))
+    fecha = Date(3, 4, 2005)
+    print(fecha.is_leap_year())
 
 if __name__ == "__main__":
     main()
